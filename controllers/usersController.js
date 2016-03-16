2ea1dda283c467f8e68bb0e81284b3beb3ba40fd
vadim <vsghostvs@gmail.com>
2016-03-16 23:44:40 +0200

    123

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,34 +1,28 @@
 "use strict";
 
-var postJson = require("post-json");
-var users = require("../lib/users");
-
-/*
-var url = "http://my-post-url.net/path"
-var body = {my: "test", data: "fun"}
-
-postJson(url, body, function (err, result) {
-    // do stuff
-}) */
+var postJson = require("post-data"),
+    users = require("../lib/users"),
+    url = require("url");
 
 
 module.exports = {
     getUsers: function (request, response, next) {
-        var result = users.getUsers();
-        next(null);
+        try {
+            var result = users.getUsers();
+        } catch (e) {
+            next(null);
+        }
     },
     postUsers: function(request, response, next){
         /* next(null) */
-        setTimeout(function(next){
             response.statusCode = 200;
             try{
-                response.write(renderer.render('hello.ect', {
-                    message: "Hello world"
-                }));
+                var testUsr = postJson(request.body, JSON.parse,  function(e) {
+                    if (error) throw error;
+                });
                 next();
             }catch(e){
                 next(e);
             }
-        }, 500, next)
-    },
+        }
 };
--- index.js
+++ index.js
@@ -2,10 +2,36 @@
 
 var http = require("http"),
     url = require("url"),
-    routes = require("./routes");
+    routes = require("./router");
 
 function requestHandler(request, response) {
 
     var parsedUrl = url.parse(request.url, true),
-        method = request.method.toLowerCase();
-}
+        methodType = request.method.toLowerCase();
+    return parsedUrl, methodType;
+
+
+
+if(routes[methodType][parsedUrl.pathname]){
+    routes[methodType][parsedUrl.pathname](request, response, function(err){
+        if(err){
+            response.statusCode = 500;
+            response.write(JSON.stringify({
+                "message": err.message,
+                "status": "Internal Server Error"
+            }));
+            response.end();
+        }else{
+            response.end();
+        }
+    });
+}else{
+    response.statusCode = 404;
+    response.end("Not Found");
+};}
+
+
+var server  = http.createServer(requestHandler);
+
+
+server.listen(3000);
